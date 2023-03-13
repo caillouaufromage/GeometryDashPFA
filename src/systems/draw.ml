c5cb75722d06c195e78295d4e8e2863a169e95c2
falves1 <akulla.pro@gmail.com>
2023-03-13 11:02:55 +0100

    Trail + collision spikes

--- src/systems/draw.ml
+++ src/systems/draw.ml
@@ -31,8 +31,9 @@ let texture_from_type (b: t) =
     | _ -> 2;;
 
 let white = Gfx.color 40 40 40 255;;
-let playerTrace = Array.make 100 Vector.{x = 0; y = 0};;
-let writeIndex = ref 0;
+let maxTrace = 20;;
+let playerTrace = Array.make maxTrace Vector.{x = 0.0; y = 0.0};;
+let writeIndex = ref 0;;
 
 let update _dt el =
   let win = Game_state.get_window () in
@@ -68,9 +69,6 @@ let update _dt el =
     let color = e # color # get in 
 
     (* à l'intérieur du cadre OU est assez grand pour traverser le cadre*)
-    if e#rot#get != 0.0 then
-      Gfx.set_transform ctx e#rot#get false false;
-  
     if (relativeX < camW && relativeX + width > 0) then
       let texture = Hashtbl.find textures (texture_from_type e) in
 
@@ -78,34 +76,38 @@ let update _dt el =
         match e#block_type#get with
           | Player -> 
             (* Player icone *)
+            if e#rot#get != 0.0 then
+              Gfx.set_transform ctx e#rot#get false false;
+              
             Gfx.blit_scale ctx win_surf (Gfx.get_resource texture) relativeX (int_of_float y) width height;
-
+              
             (* Index trace*)
             writeIndex := !writeIndex + 1;
-
-            if !writeIndex > 99 then
-              writeIndex := 0;
-
-            playerTrace.(!writeIndex) <- e#position#get
             
+            if !writeIndex >= maxTrace then
+              writeIndex := 0;
+              
+            playerTrace.(!writeIndex) <- e#position#get;
+              
             (* On doit dessiner sa trace*)
             let oldPos = ref (e#position#get) in
+            
             if e#rot#get != 0.0 then Gfx.reset_transform ctx;
-
+            
             Array.iteri (fun index a ->
-              (* Verifier bordure 100 et 0 *) 
               if(index != !writeIndex + 1) then begin
-                let oldPos = playerTrace.(if index == 0 then 99 else index) in
+                let oldPos = playerTrace.(if (index-1) < 0 then maxTrace-1 else (index-1)) in
                 let Vector.{x; y} = a in
                 Gfx.set_color ctx (Gfx.color 52 152 219 255);
-  
-                let ang = (Float.atan2 (y -. !(oldPos).y) (x -. !(oldPos).x)) *. 180.0 /. 3.141592 in
+                
+                let ang = (Float.atan2 (y -. oldPos.y) (x -. oldPos.x)) *. 180.0 /. 3.141592 in
                 Gfx.set_transform ctx ang false false;
-                Gfx.debug "%f\n" ang;
+                Gfx.debug "%d\n" index;
                 Gfx.fill_rect ctx win_surf ((int_of_float x) - cameraX + width/2) (int_of_float y + height/2) 4 4;
                 Gfx.reset_transform ctx;
               end
             ) playerTrace;
+            ();
           | Level_Solid | Solid | Spikes -> 
             (* On applique une rotation si il y a besoin*)
             let is_heightSup = width < height in
@@ -132,10 +134,6 @@ let update _dt el =
               Gfx.blit_scale ctx win_surf (Gfx.get_resource texture) (relativeX + displayInt * height) (int_of_float y) rest height;
 
           | Level_End -> ();
-    end;
-
-    if e#rot#get != 0.0 then
-      Gfx.reset_transform ctx;
-    ) el;
+    end;) el;
 
     Gfx.commit ctx
